869e868c4f9fa208ac97dbcd30f8ced82bccfc90
Lucía España <99997724+luespana@users.noreply.github.com>
2022-09-27 01:57:30 -0300

    last changes

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -1,33 +1,21 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import data from "./data.json";
 
+describe("Star Wars App", () => {
+  beforeAll(() => jest.spyOn(window, "fetch"));
 
-describe("Star Wars App", () =>{
-  beforeAll(() => jest.spy0n(window,"fetch"));
-
-  it("Sholud show a list of characters including Luke Skywalker", () =>{
-    render(<App/>);
-    expect(screen.getByText("Luke Skaywalker")).toBeInTheDocument();
-  });
-
-  it("Sholud show a list of characters from a JSON file", () =>{
-    render(<App/>);
-    for(let character of data.results){
-      expect(screen.getByText("character.name")).toBeInTheDocument();
-    }
-  });
-
-  it("Sholud show a list of characters from the API", async () =>{
+  it("Sholud show a list of characters from the API", async () => {
     window.fetch.mockResolvedValueOnce({
       ok: true,
-      json: async () => data
+      json: async () => data,
     });
-    render(<App/>);
-    expect(window.fetch).toHaveBeenCalledTimes(1)
-    expect(window.fetch).toHveBeenCalledWith("https://swapi.dev/api/people/")
+    render(<App />);
+    expect(window.fetch).toHaveBeenCalledTimes(1);
+    expect(window.fetch).toHveBeenCalledWith("https://swapi.dev/api/people/");
 
     for (let character of data.results) {
-      expect(await screen.findByText(character.name)),toBeInTheDocument()
+      expect(await screen.findByText(character.name)), toBeInTheDocument();
     }
   });
 });
--- app/src/api/people.js
+++ app/src/api/people.js
@@ -1,5 +1,5 @@
-export async function getPeople(){
-    const response = await fetch ("https://swapi.dev/api/people/")
-    const data = await response.json()
-    return data
+export async function getPeople() {
+  const response = await fetch("https://swapi.dev/api/people/");
+  const data = await response.json();
+  return data;
 }
