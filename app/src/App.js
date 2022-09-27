19f8f23871bf51d769e4612d255ccde0927df1b4
Lucía España <99997724+luespana@users.noreply.github.com>
2022-09-27 15:36:48 -0300

    last changes

--- app/src/App.js
+++ app/src/App.js
@@ -4,15 +4,24 @@ import { getPeople } from "./api/people";
 
 function App() {
   const [people, setPeople] = useState([]);
+  const [currentCharacter, setCurrentCharacter] = useState(1)
 
   useEffect(() => {
     getPeople().then((data) => setPeople(data.results));
   }, []);
 
+  useEffect(()=>{
+  }, [currentCharacter])
+
+  const showDetails = (character) =>{
+    const id = Number(character.url.split("/").slice(-2)[0])
+    setCurrentCharacter(id)
+  }
+
   return (
     <ul>
       {people.map((character) => (
-        <li key={character.name}>{character.name}</li>
+        <li key={character.name} onClick={()=> showDetails(character)}>{character.name}</li>
       ))}
     </ul>
   );
--- app/src/api/people.js
+++ app/src/api/people.js
@@ -3,3 +3,9 @@ export async function getPeople() {
   const data = await response.json();
   return data;
 }
+
+export async function getCharacter(id=1){
+  const response = await fetch("https://swapi.dev/api/people/${id}/")
+  const data = await response.json()
+  return data
+}
