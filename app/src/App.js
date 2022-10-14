441ef9bc6aebacdb92bf730606ea59d8f4db105d
Lucía España <99997724+luespana@users.noreply.github.com>
2022-10-14 16:10:12 -0300

    FIX/PEOPLE JS

--- app/src/App.js
+++ app/src/App.js
@@ -3,61 +3,63 @@ import "./App.css";
 import { getCharacter, getPeople, searchCharacter } from "./api/people";
 
 function App() {
-  const inputSearch = useRef(null)
-  const [textSearch, setTextSearch] = useState("")
+  const inputSearch = useRef(null);
+  const [textSearch, setTextSearch] = useState("");
   const [people, setPeople] = useState([]);
-  const [currentCharacter, setCurrentCharacter] = useState(1)
-  const [details, setDetails] = useState([])
+  const [currentCharacter, setCurrentCharacter] = useState(null);
+  const [details, setDetails] = useState([]);
 
   useEffect(() => {
-    getPeople().then((data) => setPeople(data.results))
-  },[]);
+    getPeople().then((data) => setPeople(data.results));
+  }, []);
 
-  useEffect(()=>{
-    getCharacter(currentCharacter).then(setDetails)
-  }, [currentCharacter])
+  useEffect(() => {
+    getCharacter(currentCharacter).then(setDetails);
+  }, [currentCharacter]);
 
-  const showDetails = (character) =>{
-    const id = Number(character.url.split("/").slice(-2)[0])
-    setCurrentCharacter(id)
-  }
+  const showDetails = (character) => {
+    const id = Number(character.url.split("/").slice(-2)[0]);
+    setCurrentCharacter(id);
+  };
 
   const onChangeTextSearch = (event) => {
-    event.preventDefault()
-    const text = inputSearch.current.value
-    setTextSearch(text)
-  }
+    event.preventDefault();
+    const text = inputSearch.current.value;
+    setTextSearch(text);
+  };
 
   const onSearchSubmit = (event) => {
-    if(event.key !== "Enter") return;
+    if (event.key !== "Enter") return;
     inputSearch.current.value = "";
-    setDetails({})
-    searchCharacter(textSearch).then((data) => setPeople(data.results))
-  }
+    setDetails([]);
+    searchCharacter(textSearch).then((data) => setPeople(data.results));
+  };
 
   return (
     <div>
-      <input 
-      ref={inputSearch} 
-      onChange={onChangeTextSearch} 
-      onKeyDown={onSearchSubmit}
-      type="text" placeholder="Busca un Personaje"/>
+      <input
+        ref={inputSearch}
+        onChange={onChangeTextSearch}
+        onKeyDown={onSearchSubmit}
+        type="text"
+        placeholder="Busca un Personaje"
+      />
       <ul>
-      {people.map((character) => (
-        <li key={character.name} onClick={()=> showDetails(character)}>{character.name}</li>
-      ))}
+        {people.map((character) => (
+          <li key={character.name} onClick={() => showDetails(character)}>
+            {character.name}
+          </li>
+        ))}
       </ul>
-      {details && (
-      <aside>
-      <h1>
-        {details.name}
-      </h1>
-      <ul>
-        <li>Height: {details.height}</li>
-        <li>Mass: {details.mass}</li>
-        <li>Year of Birth: {details.birth_year}</li>
-      </ul>
-      </aside>
+      {details && currentCharacter && (
+        <aside>
+          <h1>{details.name}</h1>
+          <ul>
+            <li>Height: {details.height}</li>
+            <li>Mass: {details.mass}</li>
+            <li>Year of Birth: {details.birth_year}</li>
+          </ul>
+        </aside>
       )}
     </div>
   );
--- app/src/api/people.js
+++ app/src/api/people.js
@@ -4,14 +4,16 @@ export async function getPeople() {
   return data;
 }
 
-export async function getCharacter(id=1){
-  const response = await fetch("https://swapi.dev/api/people/" + id)
-  const data = await response.json()
-  return data
+export async function getCharacter(id) {
+  if (id !== null) {
+    const response = await fetch("https://swapi.dev/api/people/" + id);
+    const data = await response.json();
+    return data;
+  }
 }
 
-export async function searchCharacter(name){
-  const response = await fetch("https://swapi.dev/api/people/?search=${name}")
-  const data = await response.json()
-  return data
+export async function searchCharacter(name) {
+  const response = await fetch(`https://swapi.dev/api/people/?search=${name}`);
+  const data = await response.json();
+  return data;
 }
